edf36fda8e260e626a4381080edac1fe6858945f
Joe Polny <joepolny@gmail.com>
2023-07-30 09:20:37 -0400

    catchup

--- algorun.go
+++ algorun.go
@@ -3,9 +3,12 @@ package main
 import (
 	"bufio"
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -14,6 +17,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/algorand/go-algorand-sdk/client/v2/algod"
 	"github.com/cavaliergopher/grab/v3"
 	"github.com/docopt/docopt-go"
 	"github.com/schollz/progressbar/v3"
@@ -50,6 +54,23 @@ type Config struct {
 	GoalArgs      []string `docopt:"<goal-args>"`
 }
 
+func bytesToFile(path string, data []byte) {
+	err := ioutil.WriteFile(path, data, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func fileToString(path string) string {
+	fileContent, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Convert []byte to string
+	return string(fileContent)
+}
+
 func execCmd(command string) error {
 	splitCommand := strings.Split(command, " ")
 	cmd := exec.Command(splitCommand[0], splitCommand[1:]...)
@@ -82,16 +103,33 @@ func execCmd(command string) error {
 	return nil
 }
 
+func catchup() {
+	response, err := http.Get("https://algorand-catchpoints.s3.us-east-2.amazonaws.com/channel/mainnet/latest.catchpoint")
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	goalCmd(fmt.Sprintf("node catchup %s", string(responseData)))
+}
+
 func goalCmd(args string) {
 	execCmd(fmt.Sprintf("%s -d %s %s", Props.GoalPath, Props.DataDir, args))
 }
 
 func nodeStart() {
 	goalCmd("node start")
+	goalCmd("kmd start -t 0")
 }
 
 func nodeStop() {
 	goalCmd("node stop")
+	goalCmd("kmd stop")
 }
 
 func nodeStatus() {
@@ -135,7 +173,7 @@ func getVersion(match string) (string, error) {
 	)
 
 	if err != nil {
-		log.Fatalln(err)
+		panic(err)
 	}
 
 	for i := 0; i < len(releases); i++ {
@@ -149,9 +187,11 @@ func getVersion(match string) (string, error) {
 }
 
 func createCmd(config Config, release string) {
+	nodeStop()
+	os.RemoveAll(Props.DataDir)
 	versionString, err := getVersion(release)
 	if err != nil {
-		log.Fatalln(err)
+		panic(err)
 	}
 
 	versionRegex := regexp.MustCompile(`\d+\.\d+\.\d+`)
@@ -177,15 +217,100 @@ func createCmd(config Config, release string) {
 		copy.Copy(filepath.Join(tmp_bin_dir, bin), filepath.Join(Props.BinDir, bin))
 	}
 
-	mainnet_geneis := filepath.Join(Props.TempDir, "genesis", "mainnet", "genesis.json")
-	copy.Copy(mainnet_geneis, filepath.Join(Props.DataDir, "genesis.json"))
+	// TODO: Embed genesis
+	mainnetGenesis := filepath.Join(Props.TempDir, "genesis", "mainnet", "genesis.json")
+	copy.Copy(mainnetGenesis, filepath.Join(Props.DataDir, "genesis.json"))
 
-	example_config := filepath.Join(Props.TempDir, "data", "config.json.example")
-	copy.Copy(example_config, filepath.Join(Props.DataDir, "config.json"))
+	exampleConfig := filepath.Join(Props.TempDir, "data", "config.json.example")
+	copy.Copy(exampleConfig, filepath.Join(Props.DataDir, "config.json"))
 
 	nodeStart()
-	nodeStatus()
-	nodeStop()
+	kmdDirs, err := filepath.Glob(filepath.Join(Props.DataDir, "kmd-*"))
+
+	if err != nil {
+		panic(err)
+	}
+
+	kmdNet := fileToString(filepath.Join(kmdDirs[0], "kmd.net"))
+	algodNet := fileToString(filepath.Join(Props.DataDir, "algod.net"))
+
+	kmdNetSplit := strings.Split(kmdNet, ":")
+	kmdPort := strings.TrimSpace(kmdNetSplit[len(kmdNetSplit)-1])
+
+	algodNetSplit := strings.Split(algodNet, ":")
+	algodPort := strings.TrimSpace(algodNetSplit[len(algodNetSplit)-1])
+
+	algodConfigPath := filepath.Join(Props.DataDir, "config.json")
+	kmdExampleConfigPath := filepath.Join(kmdDirs[0], "kmd_config.json.example")
+
+	var algodConfig map[string]interface{}
+	var kmdConfig map[string]interface{}
+
+	algodJson := fileToString(algodConfigPath)
+	kmdJson := fileToString(kmdExampleConfigPath)
+
+	json.Unmarshal([]byte(algodJson), &algodConfig)
+	json.Unmarshal([]byte(kmdJson), &kmdConfig)
+
+	kmdConfig["allowed_origins"] = []string{"*"}
+	kmdConfig["address"] = fmt.Sprintf("0.0.0.0:%s", kmdPort)
+
+	algodConfig["EndpointAddress"] = fmt.Sprintf("0.0.0.0:%s", algodPort)
+
+	updatedAlgodJson, err := json.MarshalIndent(algodConfig, "", "  ")
+
+	if err != nil {
+		panic(err)
+	}
+
+	updatedKmdJson, err := json.MarshalIndent(kmdConfig, "", "  ")
+
+	if err != nil {
+		panic(err)
+	}
+
+	bytesToFile(algodConfigPath, updatedAlgodJson)
+	bytesToFile(filepath.Join(kmdDirs[0], "kmd_config.json"), updatedKmdJson)
+
+	algodToken := fileToString(filepath.Join(Props.DataDir, "algod.token"))
+
+	algodClient, err := algod.MakeClient(fmt.Sprintf("http://localhost:%s", algodPort), algodToken)
+
+	if err != nil {
+		panic(err)
+	}
+
+	statusReq := algodClient.Status()
+
+	timeout := time.After(10 * time.Second)
+	tick := time.Tick(500 * time.Millisecond)
+	firstStatus, err := statusReq.Do(context.Background())
+
+	if err != nil {
+		panic(err)
+	}
+
+	startingRound := firstStatus.LastRound
+
+Loop:
+	for {
+		select {
+		case <-timeout:
+			panic("timed out waiting for node to start syncin")
+		case <-tick:
+			status, err := statusReq.Do(context.Background())
+			if err != nil {
+				panic(err)
+			}
+
+			if status.LastRound > startingRound {
+				break Loop
+			}
+		}
+	}
+
+	catchup()
+	fmt.Println("Node is now catching up to mainnet!")
 }
 
 func main() {
@@ -207,16 +332,14 @@ Options:
 
 	opts, err := docopt.ParseArgs(usage, nil, "0.1.0")
 	if err != nil {
-		log.Fatalln(err)
-		return
+		panic(err)
 	}
 
 	var config Config
 
 	err = opts.Bind(&config)
 	if err != nil {
-		log.Fatalln(err)
-		return
+		panic(err)
 	}
 
 	release := config.Release
@@ -228,7 +351,7 @@ Options:
 	Props.AlgoRunDir, err = filepath.Abs(filepath.Join(".", "test-algorun-dir"))
 
 	if err != nil {
-		log.Fatalln(err)
+		panic(err)
 	}
 
 	Props.DownloadsDir = filepath.Join(Props.AlgoRunDir, "downloads")
@@ -249,7 +372,9 @@ Options:
 
 	if config.Create {
 		createCmd(config, release)
+	} else if config.Status {
+		nodeStatus()
 	} else {
-		log.Fatalln("Unrecognized command")
+		panic("Unrecognized command")
 	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module algorun
 go 1.18
 
 require (
+	github.com/algorand/go-algorand-sdk v1.24.0
 	github.com/cavaliergopher/grab/v3 v3.0.1
 	github.com/codeclysm/extract/v3 v3.1.1
 	github.com/docopt/docopt-go v0.0.0-20180111231733-ee0de3bc6815
@@ -13,25 +14,27 @@ require (
 
 require (
 	github.com/ProtonMail/go-crypto v0.0.0-20230217124315-7d5c6f04bbb8 // indirect
+	github.com/algorand/go-codec/codec v1.1.10 // indirect
 	github.com/cloudflare/circl v1.3.3 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/h2non/filetype v1.1.3 // indirect
 	github.com/juju/errors v0.0.0-20181118221551-089d3ea4e4d5 // indirect
 	github.com/juju/loggo v1.0.0 // indirect
-	github.com/klauspost/compress v1.15.13 // indirect
+	github.com/klauspost/compress v1.16.7 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
 	github.com/ulikunitz/xz v0.5.11 // indirect
-	golang.org/x/crypto v0.7.0 // indirect
-	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/crypto v0.11.0 // indirect
+	golang.org/x/net v0.12.0 // indirect
 	golang.org/x/oauth2 v0.8.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/term v0.8.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/term v0.10.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
